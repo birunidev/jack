49046f196f782b6f0ce255a0dc93c691b77e24a1
Birunidev <birunimamak@gmail.com>
2019-08-16 10:17:18 -0700

    First

---
+++ script.js
@@ -0,0 +1,16 @@
+// Portfolio Page
+$(document).ready(function () {
+
+    $('.portfolio-content').mouseenter(function () {
+        $('.hover-content', this).css('opacity', '1');
+
+    }).mouseleave(function () {
+        $('.hover-content', this).css('opacity', '0');
+    });
+
+
+
+
+
+
+});
